f3ebc637383e7fb0c2887bcb633d2073268f7018
ErikZambeli <erik_12_nf@live.com>
2020-05-15 22:52:00 -0300

    feat: add formik and yup

--- src/components/login/loginUsers/index.tsx
+++ src/components/login/loginUsers/index.tsx
@@ -1,8 +1,30 @@
 import React from "react";
+import { withFormik, FormikProps } from "formik";
+import * as Yup from "yup";
 
 import { Container, Typography, TextField, Button } from "@material-ui/core";
 
-const LoginUsers: React.FC = () => {
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+interface MyFormProps {
+  initialEmail?: string;
+  initialPassword?: string;
+}
+
+const InnerForm = (props: FormikProps<FormValues>) => {
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+  } = props;
+
   return (
     <>
       <Container component="main" maxWidth="lg">
@@ -15,42 +37,78 @@ const LoginUsers: React.FC = () => {
               Sign in to your account to continue
             </Typography>
           </div>
-          <div className="mt-4">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
+          <form onSubmit={handleSubmit}>
+            <div className="mt-4">
+              <TextField
+                type="email"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Email"
+                name="email"
+                helperText={errors.email && touched.email && errors.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.email}
+              />
+            </div>
+            <div className="mt-4">
+              <TextField
+                type="password"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+                label="Password"
+                name="password"
+                helperText={
+                  errors.password && touched.password && errors.password
+                }
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+              />
+            </div>
+            <Button
+              type="submit"
+              variant="contained"
               fullWidth
-              id="email"
-              label="Email"
-              name="emailUser"
-            />
-          </div>
-          <div className="mt-4">
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="password"
-              label="Password"
-              name="passwordUser"
-            />
-          </div>
-          <Button
-            type="button"
-            variant="contained"
-            fullWidth
-            color="primary"
-            size="large"
-            className="mb-3 mb-md-4 mt-4"
-          >
-            Sign in
-          </Button>
+              color="primary"
+              size="large"
+              className="mb-3 mb-md-4 mt-4"
+              disabled={
+                isSubmitting ||
+                !!(errors.email && touched.email) ||
+                !!(errors.password && touched.password)
+              }
+            >
+              Sign in
+            </Button>
+          </form>
         </div>
       </Container>
     </>
   );
 };
+
+const LoginUsers = withFormik<MyFormProps, FormValues>({
+  mapPropsToValues: (props) => ({
+    email: props.initialEmail || "",
+    password: props.initialPassword || "",
+  }),
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email("Email not valid").required("Email is required"),
+    password: Yup.string()
+      .required("Password is required")
+      .min(5, "Min 5 character"),
+  }),
+
+  handleSubmit(
+    { email, password }: FormValues,
+    { props, setSubmitting, setErrors }
+  ) {
+    console.log(email, password);
+  },
+})(InnerForm);
 
 export default LoginUsers;
